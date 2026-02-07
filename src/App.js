cd6bacba69f8c11e61c9a45b4af50c16cb87499e
appletonez-create <appletonez@gmail.com>
2026-02-07 23:53:29 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import WaveMVP from './WaveMVP';
+
+function App() {
+  return <WaveMVP />;
+}
+
+export default App;
+```
+
+---
+
+## ✅ 체크리스트
+```
+[ ] GitHub 저장소 생성
+[ ] README.md 업로드 (드래그)
+[ ] WaveMVP.jsx를 src/ 폴더에 업로드 (드래그)
+[ ] 위 7개 파일 복붙해서 생성
+[ ] Vercel 배포
+[ ] 완료!
